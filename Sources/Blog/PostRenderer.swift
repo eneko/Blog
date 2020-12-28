a28fd64258e7153828e6d15e3bb21580c3c3fd47
Eneko Alonso <eneko.alonso@gmail.com>
2020-12-27 17:40:33 -0800

    Fix workflow and add front matter

--- Sources/Blog/PostRenderer.swift
+++ Sources/Blog/PostRenderer.swift
@@ -41,6 +41,7 @@ public struct PostRenderer {
             title: \(issue.title)
             desc: Issue #\(issue.number)
             permalink: /articles/:title
+            image: https://eneko-blog-media.s3-us-west-2.amazonaws.com/social-preview/issue-\(issue.number).png
             date: \(Self.formatter.string(from: issue.createdAt))
             keywords: \(keywords)
             tags: \(tags)
